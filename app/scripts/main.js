5bd9110e866575dae7dd5d584ead8c88383af7b4
khushboo <khush310@gmail.com>
2015-10-11 16:56:19 +0200

    first commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('ready!');
+//closure to avoid leaking variables
+
+(function () {
+	console.log('ready!');
+
+	var button = document.getElementById('show');
+	button.addEventListener('click', show);
+
+	function show() {
+		var text = document.getElementById('hiddentext');
+		text.classList.toggle('show');
+		if ((button.innerHTML) === 'Show More') {
+			button.innerHTML = "Show Less";
+		} else {
+			button.innerHTML = 'Show More';
+		}
+	};
+
+})();
